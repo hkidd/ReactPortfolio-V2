4564d9f4c0631a92d6fee5433bf4f6f03bc525b3
hkidd <harrisonakidd@gmail.com>
2021-10-04 14:44:12 -0500

    Initial commit with some changes.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # UpdatedReactPortfolio
-Updated React Portfolio using a Nordic Giant Template
+Updated React Portfolio using a Nordic Giant Template.  Tweaks and modifications made by me to meet my needs.
+
+<br><br>
+Overall design credit to:
+https://github.com/nordicgiant2/react-nice-resume
+
+Original creator: https://github.com/tbakerx
---
+++ src/Components/Projects.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import Zmage from "react-zmage";
+import Fade from "react-reveal";
+
+let id = 0;
+class Projects extends Component {
+  render() {
+    if (!this.props.data) return null;
+
+    const projects = this.props.data.projects.map(function (projects) {
+      let projectImage = "images/projects/" + projects.image;
+
+      return (
+        <div key={id++} className="columns projects-item">
+          <div className="item-wrap">
+            <Zmage alt={projects.title} src={projectImage} />
+            <div style={{ textAlign: "center" }}>{projects.title}</div>
+          </div>
+        </div>
+      );
+    });
+
+    return (
+      <section id="projects">
+        <Fade left duration={1000} distance="40px">
+          <div className="row">
+            <div className="twelve columns collapsed">
+              <h1>Check Out Some of My Projects!</h1>
+
+              <div
+                id="projects-wrapper"
+                className="bgrid-quarters s-bgrid-thirds cf"
+              >
+                {projects}
+              </div>
+            </div>
+          </div>
+        </Fade>
+      </section>
+    );
+  }
+}
+
+export default Projects;
