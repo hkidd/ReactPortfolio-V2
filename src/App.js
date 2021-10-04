503466ee3e920d4bebfa64aa003265a02bfa26e6
hkidd <harrisonakidd@gmail.com>
2021-10-04 15:12:04 -0500

    Added profile pic and a few other small changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import ReactGA from "react-ga";
 import $ from "jquery";
 import "./App.css";
 import Header from "./Components/Header";
@@ -13,12 +12,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      foo: "bar",
       resumeData: {}
     };
-
-    ReactGA.initialize("UA-110570651-1");
-    ReactGA.pageview(window.location.pathname);
   }
 
   getResumeData() {
--- src/Components/About.js
+++ src/Components/About.js
@@ -8,11 +8,8 @@ class About extends Component {
     const name = this.props.data.name;
     const profilepic = "images/" + this.props.data.image;
     const bio = this.props.data.bio;
-    const street = this.props.data.address.street;
     const city = this.props.data.address.city;
     const state = this.props.data.address.state;
-    const zip = this.props.data.address.zip;
-    const phone = this.props.data.phone;
     const email = this.props.data.email;
     const resumeDownload = this.props.data.resumedownload;
 
@@ -24,7 +21,7 @@ class About extends Component {
               <img
                 className="profile-pic"
                 src={profilepic}
-                alt="Nordic Giant Profile Pic"
+                alt="Harrison Kidd Profile Pic"
               />
             </div>
             <div className="nine columns main-col">
@@ -38,12 +35,8 @@ class About extends Component {
                     <span>{name}</span>
                     <br />
                     <span>
-                      {street}
-                      <br />
-                      {city} {state}, {zip}
+                      {city}, {state}
                     </span>
-                    <br />
-                    <span>{phone}</span>
                     <br />
                     <span>{email}</span>
                   </p>
@@ -51,7 +44,7 @@ class About extends Component {
                 <div className="columns download">
                   <p>
                     <a href={resumeDownload} className="button">
-                      <i className="fa fa-download"></i>Download Resume
+                      <i className="fa fa-download"></i>Download My Resume
                     </a>
                   </p>
                 </div>
